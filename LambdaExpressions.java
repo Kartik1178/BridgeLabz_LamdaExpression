84e96ce7eefce1902aac827e0b70d580d9e05c20
Kartikeya Pusapati <swagboss600@gmail.com>
2026-03-02 12:39:45 +0530

    [Kartikeya]: implemented UC7

--- LambdaExpressions.java
+++ LambdaExpressions.java
@@ -1,24 +1,27 @@
 /*
- * UC6: Validate Password Rule 2 using Lambda Expression
+ * UC7: Validate Password Rule 3 using Lambda Expression
  * This class provides validation logic for
  * user password according to the rules:
  * 1. Password must contain minimum 8 characters
  * 2. Password must contain at least 1 uppercase letter
- * Valid: Password1, Abcdefgh
- * Invalid: password, abcdefgh, pass
+ * 3. Password must contain at least 1 numeric digit
+ * Valid: Password1, Abcdefg1
+ * Invalid: password1 (no uppercase), Password (no digit), pass1 (less length)
  */
 
 import java.util.function.Predicate;
 
 public class LambdaExpressions {
 
-    // Lambda function to validate password (min 8 chars + at least 1 uppercase)
-    private static final Predicate<String> validatePasswordRule2 =
-            password -> password.matches("^(?=.*[A-Z]).{8,}$");
+    // Lambda function to validate password (min 8 chars + 1 uppercase + 1 digit)
+    private static final Predicate<String> validatePasswordRule3 =
+            password -> password.matches("^(?=.*[A-Z])(?=.*[0-9]).{8,}$");
+
     public static void main(String[] args) {
-        System.out.println(validatePasswordRule2.test("Password"));  // true
-        System.out.println(validatePasswordRule2.test("Abcdefgh"));  // true
-        System.out.println(validatePasswordRule2.test("password"));  // false (no uppercase)
-        System.out.println(validatePasswordRule2.test("pass"));      // false (less than 8)
+        System.out.println(validatePasswordRule3.test("Password1")); // true
+        System.out.println(validatePasswordRule3.test("Abcdefg1"));  // true
+        System.out.println(validatePasswordRule3.test("password1")); // false (no uppercase)
+        System.out.println(validatePasswordRule3.test("Password"));  // false (no digit)
+        System.out.println(validatePasswordRule3.test("pass1"));     // false (length < 8)
     }
 }
