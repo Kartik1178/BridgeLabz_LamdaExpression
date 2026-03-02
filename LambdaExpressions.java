586097e8b23bc0abcc7319e116cc5724a4fb8a56
Kartikeya Pusapati <swagboss600@gmail.com>
2026-03-02 12:06:03 +0530

    [Kartikeya]:implemented UC3

--- LambdaExpressions.java
+++ LambdaExpressions.java
@@ -1,25 +1,24 @@
 /*
- * UC2: Validate Last Name using Lambda Expression
+ * UC3: Validate Email using Lambda Expression
  * This class provides validation logic for
- * user last name according to the rule:
- * 1. Last name should start with a capital letter.
- * 2. It should have a minimum of 3 characters.
- * Example of valid names: "Kumar", "Sharma", "Reddy"
- * Example of invalid names: "ku", "sh", "reddy"
+ * user email according to the rule:
+ * 1. Email format like abc.xyz@bl.co.in
+ * 2. Mandatory parts: abc, bl, co
+ * 3. Optional parts: xyz and in
+ * 4. Must contain proper '@' and '.' positions
+ * Valid: abc@bl.co, abc.xyz@bl.co.in
+ * Invalid: abc@.co, abc@bl, @bl.co
  */
-
 import java.util.function.Predicate;
-
 public class LambdaExpressions {
-
-    // Lambda function to validate last name using regex
-    private static final Predicate<String> validateLastName =
-            lastName -> lastName.matches("^[A-Z][a-zA-Z]{2,}$");
-
+    // Lambda function to validate email using regex
+    private static final Predicate<String> validateEmail =
+            email -> email.matches("^[a-zA-Z0-9]+(\\.[a-zA-Z0-9]+)?@[a-zA-Z]+\\.co(\\.[a-zA-Z]{2})?$");
     public static void main(String[] args) {
-        System.out.println(validateLastName.test("Kumar"));  // true
-        System.out.println(validateLastName.test("ku"));     // false
-        System.out.println(validateLastName.test("Sharma")); // true
-        System.out.println(validateLastName.test("reddy"));  // false
+        System.out.println(validateEmail.test("abc@bl.co"));          // true
+        System.out.println(validateEmail.test("abc.xyz@bl.co.in"));   // true
+        System.out.println(validateEmail.test("abc@.co"));            // false
+        System.out.println(validateEmail.test("abc@bl"));             // false
+        System.out.println(validateEmail.test("@bl.co"));             // false
     }
 }
