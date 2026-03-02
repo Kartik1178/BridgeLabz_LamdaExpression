49af323c46fea88b8defb1bd398dc0b1d16d18b4
Kartikeya Pusapati <swagboss600@gmail.com>
2026-03-02 12:22:58 +0530

    [Kartikeya]:Implemented UC6

--- LambdaExpressions.java
+++ LambdaExpressions.java
@@ -1,22 +1,24 @@
 /*
- * UC5: Validate Password Rule 1 using Lambda Expression
+ * UC6: Validate Password Rule 2 using Lambda Expression
  * This class provides validation logic for
- * user password according to the rule:
+ * user password according to the rules:
  * 1. Password must contain minimum 8 characters
- * Valid: password, abcdefgh
- * Invalid: pass, abc12
+ * 2. Password must contain at least 1 uppercase letter
+ * Valid: Password1, Abcdefgh
+ * Invalid: password, abcdefgh, pass
  */
 
 import java.util.function.Predicate;
 
 public class LambdaExpressions {
-    // Lambda function to validate password minimum 8 characters
-    private static final Predicate<String> validatePasswordRule1 =
-            password -> password.matches("^.{8,}$");
+
+    // Lambda function to validate password (min 8 chars + at least 1 uppercase)
+    private static final Predicate<String> validatePasswordRule2 =
+            password -> password.matches("^(?=.*[A-Z]).{8,}$");
     public static void main(String[] args) {
-        System.out.println(validatePasswordRule1.test("password")); // true
-        System.out.println(validatePasswordRule1.test("abc12345")); // true
-        System.out.println(validatePasswordRule1.test("pass"));     // false
-        System.out.println(validatePasswordRule1.test("abc12"));    // false
+        System.out.println(validatePasswordRule2.test("Password"));  // true
+        System.out.println(validatePasswordRule2.test("Abcdefgh"));  // true
+        System.out.println(validatePasswordRule2.test("password"));  // false (no uppercase)
+        System.out.println(validatePasswordRule2.test("pass"));      // false (less than 8)
     }
 }
